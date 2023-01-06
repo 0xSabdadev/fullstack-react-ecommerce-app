c0f0c85a289969efb92b507877f2bc74be9f63a1
0xSabdadev <jasonalhilal@gmail.com>
2023-01-06 10:35:52 +0700

    redux setup finished

--- client/src/scenes/global/Navbar.jsx
+++ client/src/scenes/global/Navbar.jsx
@@ -4,10 +4,12 @@ import {useNavigate} from 'react-router-dom'
 import {Badge, Box, IconButton, Menu} from '@mui/material'
 import {PersonOutline, ShoppingBagOutlined, MenuOutlined, SearchOutlined} from '@mui/icons-material'
 import {shades} from '../../theme'
+import {setIsCartOpen} from '../../state'
 
 const Navbar = () => {
     const navigate = useNavigate()
     const dispatch = useDispatch()
+    const cart = useSelector(state => state.cart.cart)
 
     return (
         <Box
@@ -15,31 +17,54 @@ const Navbar = () => {
             alignItems='center'
             width='100%'
             height='60px'
-            backgroundColor='rgba(255,255,255,0.95)'
+            backgroundColor='rgba(255, 255, 255, 0.95)'
             color='black'
             position='fixed'
             top='0'
             left='0'
             zIndex='1'>
             <Box
-                onClick={() => navigate('/')}
-                sx={{'&:hover': {cursor: 'pointer'}}}
-                color={shades.secondary[500]}>
-                ECOMMER
-            </Box>
-            <Box display='flex' justifyContent='space-between' columnGap='20px' zIndex='2'>
-                <IconButton sc={{color: 'black'}}>
-                    <SearchOutlined />
-                </IconButton>
-                <IconButton sc={{color: 'black'}}>
-                    <PersonOutline />
-                </IconButton>
-                <IconButton sc={{color: 'black'}}>
-                    <ShoppingBagOutlined />
-                </IconButton>
-                <IconButton sc={{color: 'black'}}>
-                    <MenuOutlined />
-                </IconButton>
+                width='80%'
+                margin='auto'
+                display='flex'
+                justifyContent='space-between'
+                alignItems='center'>
+                <Box
+                    onClick={() => navigate('/')}
+                    sx={{'&:hover': {cursor: 'pointer'}}}
+                    color={shades.secondary[500]}>
+                    ECOMMER
+                </Box>
+                <Box display='flex' justifyContent='space-between' columnGap='20px' zIndex='2'>
+                    <IconButton sx={{color: 'black'}}>
+                        <SearchOutlined />
+                    </IconButton>
+                    <IconButton sx={{color: 'black'}}>
+                        <PersonOutline />
+                    </IconButton>
+                    <Badge
+                        badgeContent={cart.length}
+                        color='secondary'
+                        invisible={cart.length === 0}
+                        sx={{
+                            '& .MuiBadge-badge': {
+                                right: 5,
+                                top: 5,
+                                padding: '0 4px',
+                                height: '14px',
+                                minWidth: '13px',
+                            },
+                        }}>
+                        <IconButton
+                            onClick={() => dispatch(setIsCartOpen({}))}
+                            sx={{color: 'black'}}>
+                            <ShoppingBagOutlined />
+                        </IconButton>
+                    </Badge>
+                    <IconButton sx={{color: 'black'}}>
+                        <MenuOutlined />
+                    </IconButton>
+                </Box>
             </Box>
         </Box>
     )
